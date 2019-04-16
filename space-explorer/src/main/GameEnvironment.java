e0269d957cce585063e6187aeab5241281ee82f4
Yu Duan <ydu34@uclive.ac.nz>
2019-04-16 15:41:35 +1200

    Read it

--- space-explorer/src/main/GameEnvironment.java
+++ space-explorer/src/main/GameEnvironment.java
@@ -20,40 +20,65 @@ public class GameEnvironment {
 		
 		boolean crewCreation = true;
 		int parsedInput = Integer.parseInt(input);
-		int numberOfCrewMembers;
 		System.out.println("How many crew members (between 2 and 4) would you like?");
 		input = in.nextLine();
 		parsedInput = Integer.parseInt(input);
-		numberOfCrewMembers = parsedInput;
+		int crewMemberNeeded = parsedInput; 
+		/* Shows the player the types of crew and ask them to select crew members until they have 4 */
+		int crewMemberCount = 0; /* the number of crew member that has been created */
 		CREWCREATION:
 		while (crewCreation) {
 			
 			System.out.println("There are 6 different types of potential crew members: ");
 			System.out.println("\t1. Engineer");
-			System.out.println("\t2. place holder");
+			System.out.println("\t2. place holder"); /* Placeholder crew members, need to decide on them */
 			System.out.println("\t3. place holder");
 			System.out.println("\t4. place holder");
 			System.out.println("\t5. place holder");
 			System.out.println("\t6. place holder");
-			System.out.println("Enter a number to learn more about the crew member.");
+			System.out.println("Please select a crew member.");
 			input = in.nextLine();
 			parsedInput = Integer.parseInt(input);
+			String selectedCrewMember = "";
 			switch(parsedInput) {
 			case 1:
+				selectedCrewMember = "Engineer";
 				Engineer.description();
 				break;
+			case 2:
+				selectedCrewMember = "Placeholder";
+				System.out.println("Placeholder");
+				break;
+			case 3:
+				selectedCrewMember = "Placeholder";
+				System.out.println("Placeholder");
+				break;
+			case 4:
+				selectedCrewMember = "Placeholder";
+				System.out.println("Placeholder");
+				break;
+			case 5:
+				selectedCrewMember = "Placeholder";
+				System.out.println("Placeholder");
+				break;
+			case 6:
+				selectedCrewMember = "Placeholder";
+				System.out.println("Placeholder");
+				break;	
 			}
-			System.out.println("\t1. Learn about other crew members.");
-			System.out.println("\t2. Choose crew members.");
+			System.out.println("Do you want to create an " + selectedCrewMember + "? (Y/N)");
 			input = in.nextLine();
-			parsedInput = Integer.parseInt(input);
-			switch(parsedInput) {
-			case 1:
-				continue CREWCREATION;
+			if (input == "Y") {
+				System.out.println("What is the crew member's name?");
+				input = in.nextLine();
+				
 			}
-
-						
+			crewMemberCount++;
+			if (crewMemberCount == crewMemberNeeded) {
+				crewCreation = false;
+			}
 		}
+			
 		
 		
 	}
