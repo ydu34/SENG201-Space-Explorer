b45c944721156e33532983b0f72fa382477a00e6
Yu Duan <ydu34@uclive.ac.nz>
2019-05-20 19:03:46 +1200

    Update SpaceOutpost.java

--- space-explorer/src/main/SpaceOutpost.java
+++ space-explorer/src/main/SpaceOutpost.java
@@ -1,52 +1,41 @@
 package main;
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 
 /**
  * Represents a space outpost.
  */
 public class SpaceOutpost {
-	
+
 	private ArrayList<MedicalItem> medicalItems = new ArrayList<MedicalItem>();
 	private ArrayList<FoodItem> foodItems = new ArrayList<FoodItem>();
 
-	
 	public void purchaseItem(MedicalItem item, Crew crew) {
-		if (crew.getMoney() >= item.getPrice()) {
-			crew.decreaseMoney(item.getPrice());
-			crew.getMedicalItems().add(item);
-			medicalItems.remove(item);
-			System.out.println(item.getName() + " has been purchased!");
-		} else {
-			System.out.println("Not enough money!");
-		}
-	}
-	
-	public void purchaseItem(FoodItem item, Crew crew) {
-		if (crew.getMoney() >= item.getPrice()) {
-			crew.decreaseMoney(item.getPrice());
-			crew.getFoodItems().add(item);
-			foodItems.remove(item);
-			System.out.println(item.getName() + " has been purchased!");
-		} else {
-			System.out.println("Not enough money!");
-		}
+		crew.decreaseMoney(item.getPrice());
+		crew.getMedicalItems().add(item);
+		medicalItems.remove(item);
 	}
 
-	
+	public void purchaseItem(FoodItem item, Crew crew) {
+		crew.decreaseMoney(item.getPrice());
+		crew.getFoodItems().add(item);
+		foodItems.remove(item);
+	}
+
 	public ArrayList<MedicalItem> getMedicalItems() {
 		return medicalItems;
 	}
+
 	public void setMedicalItems(ArrayList<MedicalItem> medicalItems) {
 		this.medicalItems = medicalItems;
 	}
+
 	public ArrayList<FoodItem> getFoodItems() {
 		return foodItems;
 	}
+
 	public void setFoodItems(ArrayList<FoodItem> foodItems) {
 		this.foodItems = foodItems;
 	}
-	
 
-	
 }
